4dc227f52d764f53e992878043508840c01dcc58
ianfajardo <hello@ianfajardo.com>
2015-01-22 00:12:37 -0500

    Added Meet Pipe, Input Output, and Transform

--- program.coffee
+++ program.coffee
@@ -1,2 +1,28 @@
 ### Beep Boop ###
+###
 console.log "beep boop"
+###
+
+###Meet Pipe###
+###
+fs = require 'fs'
+
+file = process.argv[2]
+
+fs.createReadStream(file).pipe(process.stdout)
+###
+
+###Input Output###
+###
+process.stdin.pipe(process.stdout)
+###
+
+###Transform###
+###
+through = require 'through'
+tr = through (buf) -> 
+  this.queue(buf.toString().toUpperCase())
+
+process.stdin.pipe(tr).pipe(process.stdout)
+###
+
--- program.js
+++ program.js
@@ -2,7 +2,43 @@
 
 /* Beep Boop */
 
+
+/*
+console.log "beep boop"
+ */
+
+
+/*Meet Pipe */
+
+
+/*
+fs = require 'fs'
+
+file = process.argv[2]
+
+fs.createReadStream(file).pipe(process.stdout)
+ */
+
+
+/*Input Output */
+
+
+/*
+process.stdin.pipe(process.stdout)
+ */
+
+
+/*Transform */
+
 (function() {
-  console.log("beep boop");
+  var through, tr;
+
+  through = require('through');
+
+  tr = through(function(buf) {
+    return this.queue(buf.toString().toUpperCase());
+  });
+
+  process.stdin.pipe(tr).pipe(process.stdout);
 
 }).call(this);
